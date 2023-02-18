74a62429967fa9b55429c9fa648f6001e6f709ab
hdkef <hdkef11@gmail.com>
2023-02-18 11:05:06 +0700

    create payload JSON

--- delivery/payload-json.go
+++ delivery/payload-json.go
@@ -1,1 +1,31 @@
 package delivery
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hdkef/jameter/models"
+)
+
+func InputJSONPayload(reqs *models.ReqsWrapper) {
+	var menu int = -1
+	var jsonStr string
+	for menu != 0 {
+		//input json string
+		fmt.Printf("Input JSON")
+		_, err := fmt.Scanln(&jsonStr)
+		if err != nil {
+			fmt.Println("Invalid input")
+			continue
+		}
+
+		//validate json string
+		valid := json.Valid([]byte(jsonStr))
+		if !valid {
+			fmt.Println("JSON invalid")
+		} else {
+			menu = 0
+		}
+	}
+	reqs.Payload = models.ReqsJSON{Data: jsonStr}
+}
