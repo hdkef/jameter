1f9293c9c7c4d2badfd68a260dc8d7d1eb537d27
hdkef <hdkef11@gmail.com>
2023-02-18 16:47:52 +0700

    fix deadlock condition

--- delivery/execute-req.go
+++ delivery/execute-req.go
@@ -15,69 +15,73 @@ import (
 )
 
 func resChanListener(resultC chan *models.ResultChan, wg *sync.WaitGroup, closeC chan bool) {
+out:
 	for {
-		res := <-resultC
+		select {
+		case <-closeC:
+			close(resultC)
+			close(closeC)
+			break out
+		case res := <-resultC:
 
-		var output string
+			var output string
 
-		//handle response body according to MIME type
-		defer res.Res.Body.Close()
-		bodyBytes, err := io.ReadAll(res.Res.Body)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		var body interface{}
-		if res.Res.Header.Get("Accept") == "application/json" {
-			var jsonBody map[string]interface{}
-			err = json.Unmarshal(bodyBytes, &jsonBody)
+			//handle response body according to MIME type
+			defer res.Res.Body.Close()
+			bodyBytes, err := io.ReadAll(res.Res.Body)
 			if err != nil {
 				fmt.Print(err.Error())
 			}
-			body = jsonBody
-		} else {
-			body = string(bodyBytes)
-		}
+			var body interface{}
+			if res.Res.Header.Get("Accept") == "application/json" {
+				var jsonBody map[string]interface{}
+				err = json.Unmarshal(bodyBytes, &jsonBody)
+				if err != nil {
+					fmt.Print(err.Error())
+				}
+				body = jsonBody
+			} else {
+				body = string(bodyBytes)
+			}
 
-		output += fmt.Sprint("Date\t: ", time.Now().String(), "\n")
-		output += fmt.Sprint("Status Code\t: ", res.Res.StatusCode, "\n")
-		output += fmt.Sprint("Status Info\t: ", res.Res.Status, "\n")
+			output += fmt.Sprint("Date\t: ", time.Now().String(), "\n")
+			output += fmt.Sprint("Status Code\t: ", res.Res.StatusCode, "\n")
+			output += fmt.Sprint("Status Info\t: ", res.Res.Status, "\n")
 
-		//output response header
-		for k, v := range res.Res.Header {
-			output += fmt.Sprintf("%s\t: %s\n", k, v)
-		}
+			//output response header
+			for k, v := range res.Res.Header {
+				output += fmt.Sprintf("%s\t: %s\n", k, v)
+			}
 
-		output += fmt.Sprint("Body\t: \n", body, "\n")
-		output += "\n\n\n"
+			output += fmt.Sprint("Body\t: \n", body, "\n")
+			output += "\n\n\n"
 
-		//append output to file
+			//append output to file
 
-		fileName := fmt.Sprintf("%s_%s",
-			res.Req.Name,
-			res.Res.Request.URL.Host)
+			fileName := fmt.Sprintf("%s_%s",
+				res.Req.Name,
+				res.Res.Request.URL.Host)
 
-		fileName = strings.ReplaceAll(fileName, " ", "_")
-		fileName += ".jamet"
+			fileName = strings.ReplaceAll(fileName, " ", "_")
+			fileName += ".jamet"
 
-		fileDir := fmt.Sprintf("output/%s", fileName)
+			fileDir := fmt.Sprintf("output/%s", fileName)
 
-		f, err := os.OpenFile(fileDir, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
+			f, err := os.OpenFile(fileDir, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Print(err.Error())
+			}
 
-		defer f.Close()
-		if _, err := f.WriteString(output); err != nil {
-			fmt.Print(err.Error())
-		}
+			defer f.Close()
+			if _, err := f.WriteString(output); err != nil {
+				fmt.Print(err.Error())
+			}
 
-		//print result status
-		fmt.Printf("\nStatus\t\t: %d\n", res.Res.StatusCode)
-		fmt.Printf("Details on\t: %s\n", fileDir)
+			//print result status
+			fmt.Printf("\nStatus\t\t: %d\n", res.Res.StatusCode)
+			fmt.Printf("Details on\t: %s\n", fileDir)
 
-		wg.Done()
-		if done := <-closeC; done {
-			break
+			wg.Done()
 		}
 	}
 }
@@ -86,11 +90,12 @@ func addPayload(req *http.Request, r *models.ReqsWrapper) {
 
 }
 
-func hit(r *models.ReqsWrapper, resultC chan *models.ResultChan, wg *sync.WaitGroup, client *http.Client) {
+func hit(r models.ReqsWrapper, resultC chan *models.ResultChan, wg *sync.WaitGroup, client *http.Client) {
 	//create new request
 	req, err := http.NewRequest(r.Method, r.URI, nil)
 
 	if err != nil {
+		fmt.Println(err.Error())
 		wg.Done()
 		return
 	}
@@ -110,11 +115,12 @@ func hit(r *models.ReqsWrapper, resultC chan *models.ResultChan, wg *sync.WaitGr
 	}
 
 	//add payload
-	addPayload(req, r)
+	addPayload(req, &r)
 
 	//execute the request
 	resp, err := client.Do(req)
 	if err != nil {
+		fmt.Println(err.Error())
 		wg.Done()
 		return
 	}
@@ -123,8 +129,9 @@ func hit(r *models.ReqsWrapper, resultC chan *models.ResultChan, wg *sync.WaitGr
 
 	resultC <- &models.ResultChan{
 		Res: resp,
-		Req: r,
+		Req: &r,
 	}
+
 }
 
 func ExecuteByIDS(project *models.Project) {
@@ -158,7 +165,7 @@ func ExecuteByIDS(project *models.Project) {
 		for _, k := range project.Reqs {
 			if k.ID == v {
 				wg.Add(1)
-				go hit(&k, resultC, &wg, client)
+				go hit(k, resultC, &wg, client)
 			}
 		}
 	}
@@ -171,8 +178,6 @@ func ExecuteByIDS(project *models.Project) {
 
 	//if all executed, close listener
 	doneC <- true
-	close(resultC)
-	close(doneC)
 }
 
 func ExecuteAll(project *models.Project) {
@@ -196,7 +201,7 @@ func ExecuteAll(project *models.Project) {
 	//execute every req
 	for _, v := range project.Reqs {
 		wg.Add(1)
-		go hit(&v, resultC, &wg, client)
+		go hit(v, resultC, &wg, client)
 	}
 
 	//listener
@@ -207,8 +212,6 @@ func ExecuteAll(project *models.Project) {
 
 	//if all executed, close listener
 	doneC <- true
-	close(resultC)
-	close(doneC)
 }
 
 func ExecuteRequest(project *models.Project) (menu int) {
