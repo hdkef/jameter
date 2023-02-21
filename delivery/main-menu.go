8972aa24be6cd501f2db1aef0ffc8c42f79bc18f
hdkef <hdkef11@gmail.com>
2023-02-21 14:06:42 +0700

    remove update feature

--- delivery/main-menu.go
+++ delivery/main-menu.go
@@ -9,7 +9,7 @@ import (
 func MainMenu(project *models.Project) {
 	var menu int = -1
 	for menu != 0 {
-		fmt.Print("\n1.Read request\n2.Create request\n3.Update request\n4.Delete request\n5.Execute request\n6.Load Test\n7.Save\n8.Quit\n9.Delete project\n\nChoose menu : ")
+		fmt.Print("\n1.Read request\n2.Create request\n3.Delete request\n4.Execute request\n5.Load Test\n6.Save\n7.Quit\n8.Delete project\n\nChoose menu : ")
 		_, err := fmt.Scanln(&menu)
 		if err != nil {
 			fmt.Println("Invalid menu")
@@ -21,18 +21,16 @@ func MainMenu(project *models.Project) {
 		case 2:
 			menu = CreateRequest(project)
 		case 3:
-			menu = UpdateRequest(project)
-		case 4:
 			menu = DeleteRequest(project)
-		case 5:
+		case 4:
 			menu = ExecuteRequest(project)
-		case 6:
+		case 5:
 			menu = LoadTest(project)
-		case 7:
+		case 6:
 			menu = SaveProject(project)
-		case 8:
+		case 7:
 			menu = 0
-		case 9:
+		case 8:
 			menu = DeleteProject(project)
 		}
 	}
--- delivery/update-req.go
+++
@@ -1,7 +0,0 @@
-package delivery
-
-import "github.com/hdkef/jameter/models"
-
-func UpdateRequest(project *models.Project) (menu int) {
-	return -1
-}
